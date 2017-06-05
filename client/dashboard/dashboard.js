412c80a2775925b46302346529f6ef8050e6b037
Ajinkya <albus702@gmail.com>
2017-06-06 02:34:03 +0530

    maps for later

--- client/dashboard/dashboard.js
+++ client/dashboard/dashboard.js
@@ -2,6 +2,20 @@ import { Template } from 'meteor/templating';
 
 Template.dashboard.onRendered(function() {
   L.Icon.Default.imagePath = '/packages/bevanhunt_leaflet/images/';
-  // L.tileLayer.provider('Stamen.Watercolor').addTo(map);
   var map = L.map('map');
+  L.tileLayer.provider('Stamen.Watercolor').addTo(map);
+  
+//   var CartoDB_DarkMatterNoLabels = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png', {
+// 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+// 	subdomains: 'abcd',
+// 	maxZoom: 19
+// });
+
+//   var Stamen_Toner = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
+// 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+// 	subdomains: 'abcd',
+// 	minZoom: 0,
+// 	maxZoom: 20,
+// 	ext: 'png'
+// });
 });
